1ee0500addf6200a7d6d776d9a40af5c9e0c4181
PiotrWrotny <piotrwro01@gmail.com>
2021-12-02 20:03:52 +0100

    podstawowa struktura projektu
    podstawowa struktura wygenerowana przez VS

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace serwis_z_filmami.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
